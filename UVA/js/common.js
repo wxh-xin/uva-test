dfe963e877cb078a3dc91e9a88817152d60a70e6
Lixin <273642443@qq.com>
2017-04-07 09:54:11 +0800

    test

---
+++ UVA/js/common.js
@@ -0,0 +1,12 @@
+//
+// (function () {
+
+// 	var absolute_width_param = 54;
+//   document.addEventListener('DOMContentLoaded', function () {
+//       var html = document.documentElement;
+//       var windowWidth = html.clientWidth;
+//       html.style.fontSize = windowWidth / absolute_width_param + 'px';
+//   }, false);
+
+
+// })();
